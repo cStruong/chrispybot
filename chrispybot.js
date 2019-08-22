d55f531dacdc58d91e79c142f91a625a9781da1f
Your Name <christophertruongm@gmail.com>
2019-08-21 21:32:28 -0400

    possible iterator for each business to be in same embed

--- chrispybot.js
+++ chrispybot.js
@@ -65,9 +65,8 @@ chrispyBotClient.on("message", (message) => {
   }
 
 
-  //next func
+  //yelp function
   if (message.content.includes("_yelp") && message.author.bot === false) {
-    message.channel.send("Yelp Functionalities are in the works!")
     let query = message.content.slice(6);
     let location = query.split(" ").slice(-1);
     let queryStr = query.split(" ").pop(-1).join(" ");
@@ -82,6 +81,15 @@ chrispyBotClient.on("message", (message) => {
     })
     .then(parsedJSON => {
       console.log(parsedJSON);
+      //perhaps for each inside embed is a field for each business
+      message.channel.send({embed:{
+        color: 3447003,
+        fields:[{
+          name: parsedJSON.businesses.forEach(business => {
+            business.name
+        })}]
+      }
+      })
     })
   }
 
