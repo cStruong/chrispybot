9f4d435a808c87473c2fa9f205555f42b487658c
Your Name <christophertruongm@gmail.com>
2019-08-22 12:16:48 -0400

    fixed query string getting and location accepts strings for cities as well as zipcodes

--- chrispybot.js
+++ chrispybot.js
@@ -23,7 +23,7 @@ chrispyBotClient.on("message", (message) => {
       .catch(console.error)
       return;
     } else {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&APPID=f3fd398452f635ebe38ebbbb1141638e`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&APPID=`)
       .then(response => {
         return response.json()
       })
@@ -76,7 +76,7 @@ chrispyBotClient.on("message", (message) => {
     fetch(`https://api.yelp.com/v3/businesses/search?&term=${query}+&location=${location}`, {
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer LgCvM-b2FKmq5UU9Ku-V4gryKQyzNs0KgC4UHlZH_FG2j8o81fpNR3T-Rw_zmqEHRPu6DohhYl0iAQLyZw5KTxPy36tqACMagCo800HhYHDTjKBxGwXJLIvy3483XXYx"
+        "Authorization": "Bearer "
       }
     })
     .then(response => {
@@ -106,4 +106,4 @@ chrispyBotClient.on("message", (message) => {
 })
 
 
-chrispyBotClient.login("NTY3MzE5OTQzNDA2NjgyMTIy.XLR8yQ.p8-TGaDhHs9ptqNLOn15fR7g1Pc");
+chrispyBotClient.login("");
